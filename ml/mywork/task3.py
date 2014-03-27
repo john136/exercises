a4ddb162e4c1efd0e0ffb2264e14b2fb9eedfbca
john136 <jli136@site.uottawa.ca>
2014-03-26 21:08:29 -0400

    Update task3.py
    1. use TextBlob: Simplified Text Processing; 
    2. the code show how to load bad_deals.txt, good_deals.txt for generating a training data; 
    3. a simple naïve bayes classifier is built on the training data, and it is used for classifying cases in test_deals.txt
    
    e.g.,
    
    ...
    Shop Best Sellers at ChineseLaundry.com! bad
    If you want to change the destination url, please make sure to use http://cj.discountwatchstore.com as the domain. Thanks. good
    ...
    
    also try other classifiers...

--- ml/mywork/task3.py
+++ ml/mywork/task3.py
@@ -14,3 +14,69 @@ Also, provide comments on:
     - How did you test your classifier?
 
 """
+
+# import useful tools or package for different learning problems
+
+from __future__ import print_function
+import logging
+#import pandas as pd
+import numpy as np
+from optparse import OptionParser
+import sys
+from time import time
+
+#import pylab as pl
+
+#sys.path.append('..')
+
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.datasets import load_files
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.linear_model import RidgeClassifier
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import NearestCentroid
+from sklearn.utils.extmath import density
+from sklearn import metrics
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.lda import LDA
+from sklearn.qda import QDA
+from textblob.classifiers import NaiveBayesClassifier
+from numpy import genfromtxt, savetxt
+
+
+# 1. load training data for building a simple naive bayes classifier for text classificaiton
+
+bad_data = open('../data/bad_deals.txt','r')
+train = []
+for line in bad_data:
+    train.append((line, 'bad'))
+
+good_data = open('../data/good_deals.txt','r')
+for line in good_data:
+    train.append((line, 'good'))
+
+test_data = open('../data/test_deals.txt','r')
+test = []
+for line in test_data:
+    test.append((line))
+
+cl = NaiveBayesClassifier(train)
+
+# 2. use test_deals.txt for classification
+for text in test:
+    print(text, cl.classify(text))
+    
+# other classifiers ...
